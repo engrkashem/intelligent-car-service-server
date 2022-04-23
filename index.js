418df8548f0f323a50d532cf8ffd20a94b5a5e98
engrkashem <kashemaust@gmail.com>
2022-04-23 16:53:04 +0600

    CRD operation done except delete

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config();
 
 const app = express();
@@ -12,13 +13,50 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.on5h5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    console.log('Intelligent Car DB is Connected');
-    // perform actions on the collection object
-    client.close();
-});
+
+const run = async () => {
+    try {
+        await client.connect();
+
+        const serviceCollection = client.db('intelligentCar').collection('services');
+
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        });
+
+        //POST
+        app.post('/services', async (req, res) => {
+            const addedService = req.body;
+            const result = await serviceCollection.insertOne(addedService);
+            res.send(result);
+        });
+
+        //Delete
+        app.delete('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await serviceCollection.deleteOne(query);
+            res.send(result);
+        });
+
+    }
+    finally {
+
+    }
+};
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
