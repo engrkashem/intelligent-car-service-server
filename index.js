c89d195540982dac3528b9343996f6bcf5d465c1
engrkashem <kashemaust@gmail.com>
2022-04-23 12:32:24 +0600

    initialisation

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.on5h5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    console.log('Intelligent Car DB is Connected');
+    // perform actions on the collection object
+    client.close();
+});
+
+
+app.get('/', (req, res) => {
+    res.send('Running Intelligent Server.')
+})
+
+app.listen(port, () => {
+    console.log('Intelligent car service server running at port: ', port);
+})
